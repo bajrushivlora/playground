ec93e05b2062b7e40b7190971da794c84f8deb5b
bajrushivlora <bajrushivlora@gmail.com>
2021-11-19 06:59:36 -0500

    finished welcome page, started shop

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 var text= document.querySelector(".welcome_text");
-
+var button = document.querySelector(".welcome_button");
+var buttonText = document.querySelector(".welcome_button_text");
 
 function showText() {
     text.style.opacity = "1";
 }
 
+function showButtonText() {
+    buttonText.style.opacity = "1";
+}
+
 setTimeout(showText, 16000);
+setTimeout(showButtonText, 18000);
